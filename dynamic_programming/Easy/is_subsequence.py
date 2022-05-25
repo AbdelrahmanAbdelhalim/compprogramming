0e848cf4b72ad69ef94183067f8b301aeba9942f
Abdelrahman Abdelhalim <abdelrahmanbahaaeldin@gmail.com>
2022-05-25 20:42:07 +0100

    Finished write up for is_subsequence problem

--- dynamic_programming/Easy/is_subsequence.py
+++ dynamic_programming/Easy/is_subsequence.py
@@ -44,8 +44,22 @@ A subsequence of a string is a new string that is formed from the original strin
       We can think about the scanning as a lookup operation
       We can build a hashmap of the target strings with the keys as each unique character and the
       value as the indices of its appearance (We should precompute it once)
-    
-    4.DP (Great example when DP sucks compared to other algorithms)
+      for each subsequent look up that we do we can also look for the correct index using binary
+      search to further improve our time complexity
+
+      - Time complexity: O(|T| + |S| * log(|T|)
+        Building the hasmap takes O(|T|) times. If we were building an api this should be done in
+        the constructor of a class and therefore done once. For each further lookup: Each lookup
+        from the hashmap costs constant time but finding the suitable index would take O(|T|) for
+        for linear search of O(log|T|) with binary search. Therefore that part would be bounded
+        by O(|S|*log|T|)
+      - Space complexity: O(|T|)
+        We built a hashmap that consists of the indices for each character in the target string.
+        The size of values in the hashmap would be |T|. In the worst case scenario, we could have
+        as many keys as values. In total the space complexity of the hashmap would be O(|T|)
+
+    4.DP (Great example when DP sucks compared to other algorithms) Makes me wonder if that
+      problem should be in the DP directory at all ?
 '''
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
