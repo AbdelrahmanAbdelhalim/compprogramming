0db4802fb027c0adcdf0bca47e5026f8af190cfd
Abdelrahman Abdelhalim <abdelrahmanbahaaeldin@gmail.com>
2022-11-28 11:15:19 +0000

    rotate image solution

---
+++ 150_interview_questions/Rust/rotate_image.rs
@@ -0,0 +1,34 @@
+// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+impl Solution {
+    //Solution Performance: Runtime 1 ms Beats 66.39% Memory 2.2 MB Beats 43.44%
+    pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
+        let mut l = 0;
+        let mut r = matrix[0].len() - 1;
+        while r > l {
+            for i in 0..matrix.len() {
+                let temp = matrix[i][l];
+                matrix[i][l] = matrix[i][r];
+                matrix[i][r] = temp;
+            }
+            r -= 1;
+            l += 1;
+        }
+        let mut i = 0;
+        let mut j = 0;
+        while j < matrix.len() {
+            if i + j == matrix.len() - 1{
+                i = 0;
+                j += 1;
+                continue
+            }
+            let temp = matrix[i][j];
+            let limit = matrix.len() - 1;
+            matrix[i][j] = matrix[limit - j][limit - i];
+            matrix[limit - j][limit - i] = temp;
+            i += 1;
+        }
+    }
+}
